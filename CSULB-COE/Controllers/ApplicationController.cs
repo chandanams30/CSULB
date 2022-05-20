7151f6cc893615b9965d6ee18412ce5e727278a8
Asif Khan <asif.khan@thoughtfocus.com>
2022-05-20 10:57:47 +0530

    comments

--- CSULB-COE/Controllers/ApplicationController.cs
+++ CSULB-COE/Controllers/ApplicationController.cs
@@ -29,7 +29,7 @@ namespace CSULB_COE.Controllers
         {
             try
             {
-                // gets the application list
+                // gets the application list  
                 List<ApplicationListResponse> response = _applicationService.GetApplications(userId);
                 return Ok(response);
             }
