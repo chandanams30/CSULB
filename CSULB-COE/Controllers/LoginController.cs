e6aca4cb332e6cce5c3867b5a9a4f3b4a6a60055
vshanmugamm <vshanmugam@lynxtp.com>
2022-05-20 17:52:34 +0530

    Update LoginController.cs
    Testing

--- CSULB-COE/Controllers/LoginController.cs
+++ CSULB-COE/Controllers/LoginController.cs
@@ -25,7 +25,7 @@ namespace CSULB_COE.Controllers
         }
 
         [HttpGet("Authenticate")]
-        //public IActionResult Login ([FromBody]Models.AuthenticateRequest model) // uncomment after testing
+        //public IActionResult Login ([FromBody]Models.AuthenticateRequest model) // uncomment after testing // updated testing venky
         public IActionResult Login(string userName,string password)
         {
             //_logger.LogInformation("Start : Authenticating for {userName}",userName);
