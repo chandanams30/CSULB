fa9c89a019f000904799768066a3a55eb8654e14
Asif Khan <asif.khan@thoughtfocus.com>
2022-07-21 17:21:36 +0530

    Update FieldWorkController.cs
    fieldwork additional changes

--- CSULB-COE/Controllers/FieldWorkController.cs
+++ CSULB-COE/Controllers/FieldWorkController.cs
@@ -87,16 +87,16 @@ namespace CSULB_COE.Controllers
             //-------------just for testing - comment it after testing
             //string filepath = "D:\\CSULB\\GitHub\\Documents\\TBTEST.pdf";
             // string filepath = "D:\\CSULB\\GitHub\\Documents\\TB-TEST.docx";
-            string filepath = "D:\\CSULB\\GitHub\\Documents\\Student Clearance Form Sample.pdf";
-            byte[] fileContent = null;
-            System.IO.FileStream fs = new System.IO.FileStream(filepath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
-            System.IO.BinaryReader binaryReader = new System.IO.BinaryReader(fs);
-            long byteLength = new System.IO.FileInfo(filepath).Length;
-            fileContent = binaryReader.ReadBytes((Int32)byteLength);
-            input.FileContent = fileContent;
-            fs.Close();
-            fs.Dispose();
-            binaryReader.Close();
+            //string filepath = "D:\\CSULB\\GitHub\\Documents\\Student Clearance Form Sample.pdf";
+            //byte[] fileContent = null;
+            //System.IO.FileStream fs = new System.IO.FileStream(filepath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+            //System.IO.BinaryReader binaryReader = new System.IO.BinaryReader(fs);
+            //long byteLength = new System.IO.FileInfo(filepath).Length;
+            //fileContent = binaryReader.ReadBytes((Int32)byteLength);
+            //input.FileContent = fileContent;
+            //fs.Close();
+            //fs.Dispose();
+            //binaryReader.Close();
             //----end comment----------------------------------------
             #endregion
             try
@@ -167,6 +167,14 @@ namespace CSULB_COE.Controllers
                 _logger.LogError(ex, ex.Message);
                 return response;
             }
+        }
+
+        [HttpPost("UploadFieldWorkDocuments")]
+        public BaseResponse UploadFieldWorkDocuments()
+        {
+            BaseResponse response = new BaseResponse();
+
+            return response;
         }
 
         private string GetFileType(string fileExt)
