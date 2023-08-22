a8ce448b7e42df5448140e33081b6b40bb3d4f43
Chandana Shankaregowda <schandana@lynxtp.com>
2023-08-22 15:10:05 +0530

    Added Guests Controller

---
+++ CSULB-COE/Controllers/GuestsController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThoughtFocus.Domain.Request.GraduateProgram;
+using ThoughtFocus.Domain.Response;
+using ThoughtFocus.Domain.Response.Application;
+using ThoughtFocus.Domain.Response.GraduateProgram;
+using ThoughtFocus.Service.Interfaces;
+
+namespace CSULB_COE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuestsController : ControllerBase
+    {
+        public ILogger<GraduateProgramController> _logger;
+        public IGraduateProgramService _graduateProgramService;
+        private readonly IApplicationService _applicationService;
+        private readonly IConfiguration _configuration;
+        public GuestsController(IGraduateProgramService graduateProgramService ,
+              ILogger<GraduateProgramController> logger, IApplicationService applicationService
+            , IConfiguration configuration)
+        {
+            _logger = logger;
+            _graduateProgramService = graduateProgramService;
+            _applicationService = applicationService;
+            _configuration = configuration;
+        }
+        
+
+        [HttpGet("GetAppliedFormsByPrograms")]
+        public AppliedFormsByProgramsResponse GetAppliedFormsByPrograms(int userID, int programID,string termCode,int formStateID)
+        {
+            try
+            {
+                AppliedFormsByProgramsResponse response = _graduateProgramService.GetAppliedFormsByPrograms(userID, programID,termCode,formStateID);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                AppliedFormsByProgramsResponse response = new AppliedFormsByProgramsResponse();
+                response.IsSuccess = false;
+                response.Message = "Failed to retrieve data , please try after sometime";
+                response.StackTrace = ex.Message;
+                _logger.LogError(ex, ex.Message);
+                return response;
+            }
+        }
+
+        [HttpGet("GetFormStates")]
+        public FormStatesResponse GetFormStates(int userID)
+        {
+            try
+            {
+                FormStatesResponse response = _graduateProgramService.GetFormStates(userID);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                FormStatesResponse response = new FormStatesResponse();
+                response.IsSuccess = false;
+                response.Message = "Failed to retrieve data , please try after sometime";
+                response.StackTrace = ex.Message;
+                _logger.LogError(ex, ex.Message);
+                return response;
+            }
+        }
+
+        [HttpGet("GetSemesterList")]
+        public SemesterListResponse GetSemesterList()
+        {
+            try
+            {
+                SemesterListResponse response = _graduateProgramService.GetSemesterList();
+                return response;
+            }
+            catch (Exception ex)
+            {
+                SemesterListResponse response = new SemesterListResponse();
+                response.IsSuccess = false;
+                response.Message = "Failed to retrieve data , please try after sometime";
+                response.StackTrace = ex.Message;
+                _logger.LogError(ex, ex.Message);
+                return response;
+            }
+        }
+
+        [HttpGet("GetForm")]
+        public GraduateProgramFormResponse GetForm(int userID,int formID,int programID,string termCode)
+        {
+            try
+            {
+                GraduateProgramFormResponse response = _graduateProgramService.GetForm(userID,formID,programID,termCode);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                GraduateProgramFormResponse response = new GraduateProgramFormResponse();
+                response.IsSuccess = false;
+                response.Message = "Failed to retrieve data , please try after sometime";
+                response.StackTrace = ex.Message;
+                _logger.LogError(ex, ex.Message);
+                return response;
+            }
+        }
+
+        
+        [HttpGet("GetFormStudentMessageBoard")]
+        public StudentMessageBoardResponse GetFormStudentMessageBoard(int UserID, int FormID, int ProgramID, string TermCode)
+        {
+            try
+            {
+                StudentMessageBoardResponse response = new StudentMessageBoardResponse();
+                response = _graduateProgramService.GetFormStudentMessageBoard(UserID, FormID, ProgramID, TermCode);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                StudentMessageBoardResponse response = new StudentMessageBoardResponse();
+                response.IsSuccess = false;
+                response.Message = "Failed to retrieve data , please try after sometime";
+                response.StackTrace = ex.Message;
+                _logger.LogError(ex, ex.Message);
+                return response;
+            }
+        }
+        [HttpGet("GetApplicationList")]
+        public IActionResult GetApplicationList(int userId)
+        {
+            try
+            {
+                #region Below code is to pull the claims from token , currently pulling just the UserID -------------------
+                //var user = User as ClaimsPrincipal;
+                //string userIdFromToken = user.Claims.Where(c => c.Type == "UserID")
+                //    .Select(x => x.Value).FirstOrDefault();
+                // gets the application list  
+                #endregion
+
+                List<ApplicationListResponse> response = _applicationService.GetApplications(userId);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return BadRequest();
+            }
+
+        }
+    }
+}
