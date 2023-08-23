80647c88509f110c81134e3c951a816f3d70acf1
Chandana Shankaregowda <schandana@lynxtp.com>
2023-08-23 12:29:33 +0530

    Added SaveForm endpoint.

--- CSULB-COE/Controllers/GuestsController.cs
+++ CSULB-COE/Controllers/GuestsController.cs
@@ -152,5 +152,24 @@ namespace CSULB_COE.Controllers
             }
 
         }
+        [HttpPost("SaveForm")]
+        public BaseResponse SaveForm(FormSaveRequest input)
+        {
+            try
+            {
+
+                BaseResponse response = _graduateProgramService.SaveForm(input);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                BaseResponse response = new BaseResponse();
+                response.IsSuccess = false;
+                response.Message = "Failed to save data , please try after sometime";
+                response.StackTrace = ex.Message;
+                _logger.LogError(ex, ex.Message);
+                return response;
+            }
+        }
     }
 }
