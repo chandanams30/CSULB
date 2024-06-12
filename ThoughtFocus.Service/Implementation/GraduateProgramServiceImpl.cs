1843b59aca9b499375276a73081c93bd9d90344f
Chandana Shankaregowda <schandana@lynxtp.com>
2024-06-12 12:18:09 +0530

    Add Email and AlternateEamil in GetAppliedFormsByPrograms to display it on Export CSV.

--- ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
+++ ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
@@ -223,7 +223,9 @@ namespace ThoughtFocus.Service.Implementation
                                                   ReviewerRecommendation = Convert.ToString(row["ReviewerRecommendation"]),
                                                   FinalDecision = Convert.ToString(row["FinalDecision"]),
                                                   WaitlistNumber = Convert.ToInt32(row["WaitlistNumber"]),
-                                                  ShowBulkCheckBox = Convert.ToBoolean(row["ShowBulkCheckBox"])
+                                                  ShowBulkCheckBox = Convert.ToBoolean(row["ShowBulkCheckBox"]),
+                                                  Email = Convert.ToString(row["Email"]),
+                                                  AlternateEmail = Convert.ToString(row["AlternateEmail"])
                                               }).ToList();
                     }
                     if (dsAppliedFormsByProgram.Tables[1].Rows.Count > 0)
