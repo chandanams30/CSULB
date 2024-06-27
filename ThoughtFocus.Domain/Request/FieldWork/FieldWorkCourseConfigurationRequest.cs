9019da97b54c1cb5468c6f26c5293464881628e5
Chandana Shankaregowda <schandana@lynxtp.com>
2024-06-27 20:00:41 +0530

    Add endpoints for designate course: Prerequisites, type of logging, hours, and dropdown for activity logging.
    (cherry picked from commit 7b216ac04a13aff5d37b801ac8aec0f220dcd921)
    (cherry picked from commit d5c726bc4ac36fe01d339ed6b9dd36c6372e7ec8)

---
+++ ThoughtFocus.Domain/Request/FieldWork/FieldWorkCourseConfigurationRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ThoughtFocus.Domain.Response;
+
+namespace ThoughtFocus.Domain.Request.FieldWork
+{
+    public class FieldWorkCourseConfigurationRequest
+    {
+        public int CourseId { get; set; }
+        public bool EnableActivityLog { get; set; }
+        public bool AutoCompute { get; set; }
+        public bool RecordByDate { get; set; }
+        
+        public int CategoryID { get; set; }
+        public int FieldWorkHours { get; set; }
+
+        public List<FieldWorkDocumentConfig> DocumentConfig { get; set; }
+    }
+    public class FieldWorkDocumentConfig
+    {
+      
+        public int CourseId { get; set; }
+        public int DocumentID { get; set; }
+        public bool IsRestricted { get; set; }
+        public bool IsRequiredPrerequisite { get; set; }
+    }
+}
---
+++ ThoughtFocus.Domain/Response/FieldWork/FieldWorkCourseConfigurationResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThoughtFocus.Domain.Response.FieldWork
+{
+    public class FieldWorkCourseConfiguration : BaseResponse
+    {
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public string CourseNumber { get; set; }
+        public string ClassSection { get; set; }
+        public bool EnableActivityLog { get; set; }
+        public bool AutoCompute { get; set; }
+        public bool FieldWorkHours { get; set; }
+        public int CategoryID { get; set; }
+        public bool RecordByDate { get; set; }
+        public List<FieldWorkDocuments> DocumentsConfig { get; set; }
+    }
+    public class FieldWorkDocuments
+    {
+        public string DocumentName { get; set; }
+        public int DocumentId { get; set; }
+        public bool IsRestricted { get; set; }
+        public bool isRequiredPrerequisite { get; set; }
+    }
+}
