de46d86afe53b0bdcfa4894407cb4e17cef13604
Asif Khan <asif.khan@thoughtfocus.com>
2025-03-04 10:48:35 +0530

    Rates Validation
    (cherry picked from commit cc7ed27655d9a469e42dbda297a6cf75af9b6906)

--- ThoughtFocus.Service/Implementation/TravelServiceImpl.cs
+++ ThoughtFocus.Service/Implementation/TravelServiceImpl.cs
@@ -186,7 +186,7 @@ namespace ThoughtFocus.Service.Implementation
                                                Miles = row["Miles"] == DBNull.Value ? 0m : Convert.ToDecimal(row["Miles"]),
                                                ModifiedBy = Convert.ToInt32(row["ModifiedBy"]),
                                                ModifiedDateTime = Convert.ToDateTime(row["ModifiedDateTime"]),
-                                               Rate = Convert.ToDecimal(row["Rate"])
+                                               Rate = Convert.ToDecimal(row["Rate"] == DBNull.Value ? null : row["Rate"])
 
                                            }).ToList();
 
