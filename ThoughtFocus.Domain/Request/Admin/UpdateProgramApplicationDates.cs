c33d6da8a41760005ad26dcd8662b868acc7fe55
TFCORP\106531 <chandana.shankaregowda@thoughtfocus.com>
2025-04-02 11:31:16 +0530

    Added GetFutureList implementation and few classes to Admin for UAT.

---
+++ ThoughtFocus.Domain/Request/Admin/UpdateProgramApplicationDates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThoughtFocus.Domain.Request.Admin
+{
+    public class UpdateProgramApplicationDates
+    {
+        public List<int> ProgramID { get; set; }
+        public string TermCode { get; set; }
+        public DateTime ApplicationOpens { get; set; }
+        public DateTime ApplicationDeadline { get; set;}
+        public DateTime ApplicationCloseDate { get; set; }
+        public bool Status { get; set; }
+    }
+    
+}
--- ThoughtFocus.Domain/Response/Admin/RolesListResponse.cs
+++ ThoughtFocus.Domain/Response/Admin/RolesListResponse.cs
@@ -13,4 +13,30 @@ namespace ThoughtFocus.Domain.Response.Admin
         public int RoleID { get; set; }
         public string RoleName { get; set; }
     }
+    public class UpcomingSemesterListResponse : BaseResponse
+    {
+        public List<SemesterTerm> SemesterTerms { get; set; }
+    }
+
+    public class SemesterTerm
+    {
+        public string TermCode { get; set; }
+        public string TermName { get; set; }
+    }
+    public class ApplicationProgramListResponse : BaseResponse
+    {
+        public List<ApplicationProgramsList> ApplicationProgramList { get; set; }
+    }
+    public class ApplicationProgramsList
+    {
+        public int ProgramID { get; set; }
+        public string ProgramName { get; set; }
+    }
+    public class ApplicationDatesResponse : BaseResponse
+    {
+        public DateTime ApplicationOpenDate { get; set; }
+        public DateTime ApplicationCloseDate { get; set; }
+        public DateTime ApplicationDeadlineDate { get; set; }
+
+    }
 }
