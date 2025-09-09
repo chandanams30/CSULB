c22192079c9808e0a4cca70d7fa481dd6eb118be
Akhil Reddy <akhil.reddy@thoughtfocus.com>
2025-09-09 17:52:29 +0530

    Add Personal Statment

--- ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
+++ ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
@@ -227,7 +227,8 @@ namespace ThoughtFocus.Service.Implementation
                                                   Email = Convert.ToString(row["Email"]),
                                                   AlternateEmail = Convert.ToString(row["AlternateEmail"]),
                                                   ResumeUploadStatus = Convert.ToString(row["ResumeUploadStatus"]),
-                                                  SOPUploadStatus = Convert.ToString(row["SOPUploadStatus"])
+                                                  SOPUploadStatus = Convert.ToString(row["SOPUploadStatus"]),
+                                                  PersonalStatementDoc = Convert.ToString(row["PersonalStatementDoc"])
                                               }).ToList();
                     }
                     if (dsAppliedFormsByProgram.Tables[1].Rows.Count > 0)
