d5adfc1086d5f54769756681e6680d382148808e
Ramya <ramyashree.suresh@thoughtfocus.com>
2026-02-06 15:48:10 +0530

    Block emails for 'Not Offered' for graduate

--- ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
+++ ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
@@ -1150,9 +1150,9 @@ namespace ThoughtFocus.Service.Implementation
                 if (dsRec.Tables[0].Rows.Count > 0 && dsRec.Tables[1].Rows.Count > 0)
                 {
                     int applicationTypeID = Convert.ToInt32(dsRec.Tables[1].Rows[0]["ApplicationTypeID"]);
-                    if (applicationTypeID == 2)
+                    if (applicationTypeID == 2 && formStateID == 10)
                     {
-                        //if (formStateID == 10 && applicationTypeID == 3)
+                        //if (formStateID == 10 )
                         //{
                             // applicant mail
                             string logoText = "cid:myImageID";
