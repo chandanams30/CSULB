27067572c366e518a589c396034a8b93d9e85193
TFCORP\106531 <chandana.shankaregowda@thoughtfocus.com>
2026-04-08 11:18:19 +0530

    Logo changes
    (cherry picked from commit 95b722931e7ac5b3b09f3793f26ec36a332c5509)

--- ThoughtFocus.Service/Implementation/FieldWorkServiceImpl.cs
+++ ThoughtFocus.Service/Implementation/FieldWorkServiceImpl.cs
@@ -1978,7 +1978,7 @@ namespace ThoughtFocus.Service.Implementation
         {
             byte[] pdfFileContent = null;
             string evaluationTemplateBody = string.Empty;
-            string logoPath = Path.GetFullPath("SupportFiles/Img/logo.jpg");
+            string logoPath = Path.GetFullPath("SupportFiles/Img/logo.jpeg");
             jsonString = jsonString.Replace("+", " ");
             JObject schema = JObject.Parse(jsonString);
 
@@ -2192,7 +2192,7 @@ namespace ThoughtFocus.Service.Implementation
         {
             byte[] inputStream = null;
             StringBuilder sbLogData = new StringBuilder();
-            string logoPath = Path.GetFullPath("SupportFiles/Img/logo.jpg");
+            string logoPath = Path.GetFullPath("SupportFiles/Img/logo.jpeg");
             string studentName = string.Empty;
             string courseTitle = string.Empty;
             string course = string.Empty;
