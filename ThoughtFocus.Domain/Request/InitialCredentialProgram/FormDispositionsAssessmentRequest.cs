b0b2ed445c1b00a47c6957e994074ea0df72bfb0
TFCORP\106531 <chandana.shankaregowda@thoughtfocus.com>
2026-05-05 16:19:48 +0530

    Add Identifier to GetFormSubSectionAttachmentList

--- ThoughtFocus.Domain/Request/InitialCredentialProgram/FormDispositionsAssessmentRequest.cs
+++ ThoughtFocus.Domain/Request/InitialCredentialProgram/FormDispositionsAssessmentRequest.cs
@@ -31,6 +31,7 @@ namespace ThoughtFocus.Domain.Request.InitialCredentialProgram
         public int ProgramID { get; set; }
         public string TermCode { get; set; }
         public string SubSectionIdentifiers { get; set; }
+        public string Identifier { get; set; }
     }
 
     public class UpsertFormSubSectionRequest
