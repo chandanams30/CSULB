09b143b4e4557c9935a638e88de2b2cb02d560f6
TFCORP\106531 <chandana.shankaregowda@thoughtfocus.com>
2026-05-11 17:31:40 +0530

    Add Document Number to CTC prerequisite document.
    (cherry picked from commit fb95c70ede1bf0dfd28f6bdbac01ba9dd43b71c7)

--- ThoughtFocus.Service/Implementation/InitialCredentialProgramService.cs
+++ ThoughtFocus.Service/Implementation/InitialCredentialProgramService.cs
@@ -848,7 +848,8 @@ namespace ThoughtFocus.Service.Implementation
                                                   DocumentStatus = Convert.ToString(row["DocumentStatus"]),
                                                   DocumentInfo = Convert.ToString(row["DocumentInfo"]),
                                                   CanUpload = Convert.ToBoolean(row["CanUpload"]),
-                                                  CanValidate = Convert.ToBoolean(row["CanValidate"])
+                                                  CanValidate = Convert.ToBoolean(row["CanValidate"]),
+                                                  DocumentNumber = Convert.ToInt32(row["DocumentNumber"] == DBNull.Value ? null : row["DocumentNumber"])
                                               }).ToList();
 
 
