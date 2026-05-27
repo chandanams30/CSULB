88c012ba8f8d51441296d6bceb9032eed68e4c4e
TFCORP\106531 <chandana.shankaregowda@thoughtfocus.com>
2026-05-27 12:21:16 +0530

    Disable Offered mails in ITCP.

--- ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
+++ ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
@@ -1171,7 +1171,7 @@ namespace ThoughtFocus.Service.Implementation
                 if (dsRec.Tables[0].Rows.Count > 0 && dsRec.Tables[1].Rows.Count > 0)
                 {
                     int applicationTypeID = Convert.ToInt32(dsRec.Tables[1].Rows[0]["ApplicationTypeID"]);
-                    if (applicationTypeID == 1 || applicationTypeID == 2)
+                    if (applicationTypeID == 2)
                     {
                         if (formStateID == 10)
                         {
