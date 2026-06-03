80f03c24aa4077d23afc600a0c0f79842d9e8c56
TFCORP\106531 <chandana.shankaregowda@thoughtfocus.com>
2026-06-03 16:54:20 +0530

    Change date format and display program selected value for Admit letters mails
    (cherry picked from commit fc23a54f11c6eeb99befcb256e05ce88c29e8a6b)

--- ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
+++ ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
@@ -1189,7 +1189,8 @@ namespace ThoughtFocus.Service.Implementation
                             string decisionType = string.Empty;
                             string beforeBody = string.Empty;
                             string afterBody = string.Empty;
-                            string date = DateTime.Now.ToString("MM-dd-yyyy");
+                            string programSelected = string.Empty;
+                            string date = DateTime.Now.ToString("MMMM d, yyyy");
                             decisionType = "Offered";
 
                             applicantsName = Convert.ToString(dsRec.Tables[0].Rows[0]["ApplicantName"]);
@@ -1197,7 +1198,8 @@ namespace ThoughtFocus.Service.Implementation
                             ccMail = Convert.ToString(dsRec.Tables[0].Rows[0]["altEmail"]);
                             programName = Convert.ToString(dsRec.Tables[0].Rows[0]["programName"]);
                             finalDecision = Convert.ToString(dsRec.Tables[0].Rows[0]["FinalDecision"]);
-                           
+                            programSelected = Convert.ToString(dsRec.Tables[0].Rows[0]["ProgramSelected"]);
+
                             SqlParameter[] parameters2 ={
                                             new SqlParameter("@ProgramIdentifier", SqlDbType.NVarChar, 10) { Value = "" },
                                             new SqlParameter("@OfferedCategories", SqlDbType.NVarChar , 50) { Value = finalDecision },
@@ -1274,7 +1276,8 @@ namespace ThoughtFocus.Service.Implementation
                                        .Replace("[[programName]]", programName)
                                        .Replace("[[finalDecision]]", finalDecision)
                                        .Replace("[[CSULBID]]", csulbid)
-                                       .Replace("[[date]]", date);
+                                       .Replace("[[date]]", date)
+                                       .Replace("[[ProgramSelected]]", programSelected);
                             byte[] inputStr = null;
                             _sendMail.SendEmail(toMail, ccMail, "COMMON", subject, body, inputStr);
                         }
