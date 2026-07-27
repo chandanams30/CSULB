fc49ae59680add439f3f435faca3f2c046bd6bdb
chandana.shankaregowda <chandana.shankaregowda@thoughtfocus.com>
2026-07-27 16:40:44 +0530

    Add WritingSample,Optional document and Confirm program option applying to value in graduate grid.
    (cherry picked from commit 406edd5c472d0f265552f24bca7cd2290db1eb8b)

--- ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
+++ ThoughtFocus.Service/Implementation/GraduateProgramServiceImpl.cs
@@ -233,7 +233,10 @@ namespace ThoughtFocus.Service.Implementation
                                                   SOPUploadStatus = Convert.ToString(row["SOPUploadStatus"]),
                                                   PersonalStatementDoc = Convert.ToString(row["PersonalStatementDoc"]),
                                                   UniversityApplicationDetails = Convert.ToString(row["UniversityApplicationDetails"]),
-                                                  DocumentNumber = Convert.ToString(row["DocumentNumber"])
+                                                  DocumentNumber = Convert.ToString(row["DocumentNumber"]),
+                                                  WritingSampleStatus = Convert.ToString(row["WritingSampleStatus"]),
+                                                  OptionalStatus = Convert.ToString(row["OptionalStatus"]),
+                                                  ConfirmProgramOptionApplyingTo = Convert.ToString(row["ConfirmProgramOptionApplyingTo"])
                                               }).ToList();
                     }
                     if (dsAppliedFormsByProgram.Tables[1].Rows.Count > 0)
